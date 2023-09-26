ea429640492218258d8be0a74f3ffce12269a346
KrazyPotatooo <krazymix95@gmail.com>
2023-09-26 19:28:42 +0800

    practice2

--- Process.php
+++ Process.php
@@ -1,30 +1,43 @@
 <?php
+
    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $text = $_POST['text'];
-    $sort= $_POST['sort'];
+    $input_text = $_POST['text'];
+    $pattern= $_POST['sort'];
     $limit = $_POST['limit'];
    }
-    function calculate_Word_Frequency($text, $sort, $limit) {
-        $words = str_word_count($text, 1);
+
+    function calculate_Word_Frequency($input_text, $pattern, $limit) {
+        $words = str_word_count($input_text, 1);
         $stopWords = ["the", "and", "in"];
         $wordCounts = array_count_values(array_diff($words, $stopWords));
-        if ($sort === "asc") {
+        if ($pattern === "asc") 
+        {
             asort($wordCounts);
-        } else {
+        } 
+        else 
+        {
             arsort($wordCounts);
         }
+        
         $wordCounts = array_slice($wordCounts, 0, $limit);
         return $wordCounts;
     }
-        if (!empty($text)) {
-            $wordFrequencies = calculate_Word_Frequency($text, $sort, $limit);
-            echo "<h2>Word Frequency:</h2>";
-            echo "<ul>";
-            foreach ($wordFrequencies as $word => $frequency) {
-                echo "<li>$word: $frequency</li>";
-            }
-            echo "</ul>";
-        } else {
-            echo "<p>Please enter some text.</p>";
+
+    if (!empty($input_text)) 
+    {
+        $i = 0;
+        $wordFrequencies = calculate_Word_Frequency($input_text, $pattern, $limit);
+        echo "<h2>Word Frequency:</h2>";
+        echo "<ul>";
+
+        foreach ($wordFrequencies as $word => $frequency) {
+            echo "<li>$word: $frequency</li>";
         }
+
+        echo "</ul>";
+    } 
+    
+    else {
+        echo "<p>Please enter some text.</p>";
+    }
 ?>
